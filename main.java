db5740a1b63d0165df25c019bf9e77353cb4562b
Radkarl <100481043+Radkarl@users.noreply.github.com>
2022-11-23 14:20:06 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,22 @@
+
+
+public class main {
+    
+    public void start() {
+
+        System.out.println("Please log in.");
+        
+        // have user log in here
+
+        // give admin users special privileges
+
+        //// once logged in:
+        // display movies + showtimes
+        // have user select a movie and showtime
+        // have user select seats
+        // have user purchase ticket(s)
+        // allow user to write review
+
+    }
+
+}
