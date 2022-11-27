b2e1d7fc24fddab0c9980e2618c5aae34087b21c
Radkarl <100481043+Radkarl@users.noreply.github.com>
2022-11-26 22:29:02 -0600

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -1,14 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Movie {
     
     // MEMBERS
-    public int movie_id;
-    public String movie_name;
-    public float movie_runtime;
-    public String movie_type;
+    private int movie_id;
+    private String movie_name;
+    private String movie_runtime;
+    private String movie_type;
     // END MEMBERS
 
     // METHODS
-    public void searchmovie(){
+    public String searchmovie(int id){
+        String needed_movie = "";
+        BufferedReader reader;
+        try {
+			reader = new BufferedReader(new FileReader(
+					"movies.txt"));
+			String line = reader.readLine();
+            line = reader.readLine();
+			while (Integer.valueOf(line.substring(0,1)) != id) {
+				
+				// read next line
+				line = reader.readLine();
+			}
+            String[] str = line.split("\\. "); 
+            String[] str1 = str[1].split("\\, ");
+            //System.out.println("Displaying " + str1[0] + " Showtimes:");
+			reader.close();
+            needed_movie = str1[0];
+            return needed_movie;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        return needed_movie;
     }
     public void set_id(int movie_id){
         this.movie_id = movie_id;
@@ -16,7 +43,7 @@ public class Movie {
     public void set_name(String movie_name) {
         this.movie_name = movie_name;
     }
-    public void set_runtime(float movie_runtime) {
+    public void set_runtime(String movie_runtime) {
         this.movie_runtime = movie_runtime;
     }
     public void set_type(String movie_type) {
@@ -28,7 +55,7 @@ public class Movie {
     public String get_name() {
         return movie_name;
     }
-    public float get_runtime() {
+    public String get_runtime() {
         return movie_runtime;
     }
     public String get_type() {
@@ -37,3 +64,4 @@ public class Movie {
     // END METHODS
 
 }
+
