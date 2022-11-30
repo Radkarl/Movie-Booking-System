25b6716302bd62160bfcf21fa4d4faa9865823ab
edwin.perez <116044449+3dwin-perez@users.noreply.github.com>
2022-11-29 23:21:53 -0600

    Add files via upload
    This will call Customer class in order to be used in main so Customer can register when menu is created

---
+++ Register.java
@@ -0,0 +1,12 @@
+private void register()
+	{
+		System.out.println("Register Customer.\n");
+		User user = new User();
+		
+		user.RegisterUser();
+		
+		System.out.println("Customer has been registered! Welcome to Movie Booking System");
+		
+		
+		
+	}
